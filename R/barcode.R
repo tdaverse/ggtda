258bf9e850203e9ed57f4abf6d3661767901de67
Jason Cory Brunson <cornelioid@gmail.com>
2019-01-02 17:27:46 -0500

    bare-bones barcode geom

--- R/barcode.R
+++ R/barcode.R
@@ -3,6 +3,7 @@
 #' Plots a topological barcode.
 #'
 
+#' @name barcode
 #' @import ggplot2
 #' @family TDA plot layers
 #' @seealso [ggplot2::layer()] for additional arguments.
@@ -13,8 +14,12 @@
 #'   plotted in grey (regardless of the color scheme).
 #' @param ... Additional arguments passed to [ggplot2::layer()].
 #' @param geom The geometric object to use display the data.
-#'   Defaults to `line`; override the default.
+#'   Defaults to `segment`; pass a string to override the default.
+#' @param stat The The statistical transformation to use on the data.
+#'   Defaults to `identity`; pass a string to override the default.
 #' @example inst/examples/ex-barcode.r
+
+#' @rdname barcode
 #' @export
 stat_barcode <- function(mapping = NULL,
                          data = NULL,
@@ -78,6 +83,54 @@ StatBarcode <- ggproto(
     data$yend <- data$y
     
     # return the transformed data frame
+    data
+  }
+)
+
+#' @rdname barcode
+#' @export
+geom_barcode <- function(mapping = NULL,
+                         data = NULL,
+                         stat = "identity",
+                         position = "identity",
+                         na.rm = FALSE,
+                         show.legend = NA,
+                         inherit.aes = TRUE,
+                         ...) {
+  layer(
+    geom = GeomBarcode,
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      ...
+    )
+  )
+}
+
+#' @rdname ggtda-ggproto
+#' @usage NULL
+#' @export
+GeomBarcode <- ggproto(
+  "GeomBarcode", GeomSegment,
+  
+  required_aes = c("x", "xend", "group"),
+  
+  # pre-process of the data set
+  setup_data = function(data, params) {
+    
+    # introduce categorical y-values in order of `group`, `x`, and `xend`
+    data$y <- as.integer(interaction(
+      data$group, data$x, data$xend,
+      drop = TRUE, lex.order = TRUE
+    ))
+    data$yend <- data$y
+    
+    # return the pre-processed data set
     data
   }
 )
--- inst/examples/ex-barcode.R
+++ inst/examples/ex-barcode.R
@@ -4,6 +4,11 @@ data <- data.frame(
   death = c(5, 3, 5, 3),
   dim = c("0", "0", "2", "1")
 )
+# using the stat layer
 ggplot(data,
        aes(xmin = birth, xmax = death, group = dim, color = dim)) +
   stat_barcode()
+# using the geom layer
+ggplot(data,
+       aes(x = birth, xend = death, group = dim, color = dim)) +
+  geom_barcode()
