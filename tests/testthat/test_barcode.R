4c97533cca1112e5ac9b2d28ab8e9d01998f8cfc
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-01-06 11:45:34 -0500

    Added code coverage for barcodes (admittedly they don't do much yet)

---
+++ tests/testthat/test_barcode.R
@@ -0,0 +1,25 @@
+context("Test visualization via persistence barcodes")
+library("ggplot2")
+library("ggtda")
+
+test_that("Persistence barcode geom(s) and stat(s) work correctly", {
+  # toy example
+  test.df <- data.frame(
+    birth = c(0, 0, 1, 2),
+    death = c(5, 3, 5, 3),
+    dim = c("0", "0", "2", "1")
+  )
+  
+  # build ggplot2 objects without plotting
+  test.geom <- ggplot(data = test.df,
+                      aes(xmin = birth, xmax = death, group = dim, color = dim)) +
+    geom_barcode()
+  test.stat <- ggplot(data = test.df,
+                      aes(xmin = birth, xmax = death, group = dim, color = dim)) +
+    stat_barcode()
+  
+  # run tests - not doing much, just making sure there's no errors
+  #   before this point
+  expect_true(is.ggplot(test.geom))
+  expect_true(is.ggplot(test.stat))
+})
