00102a0b2a2e8bca310a010c365fc4c029c45236
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-01-06 10:45:36 -0500

    Added documentation for dataset

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' 2-dimensional point cloud of an annulus
+#' 
+#' A dataset containing the Cartesian coordinates of 100 points selected
+#' from a 2-dimensional annulus (noisy unit circle).
+#' 
+#' @format a matrix with 100 rows and 2 columns: the x- and y-coordinates
+#' @source \url{https://github.com/rrrlw/ggtda/blob/master/data-raw/annulus2d.R}
+"annulus2d"
