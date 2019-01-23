f2719ea6599c1cb01f6c5f8fe7794d811ac9438d
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-01-23 10:45:05 -0500

    Fixed function name in examples

--- inst/examples/ex-persist.R
+++ inst/examples/ex-persist.R
@@ -6,49 +6,39 @@ toy.data <- data.frame(
   dim = c("0", "0", "2", "1", "1")
 )
 
-# persistence diagram
+# flat persistence diagram
 ggplot(toy.data,
        aes(start = birth, end = death, colour = dim, shape = dim)) +
-  geom_persistence()
-# flat persistence diagram using the `stat_flat` layer method
+  geom_persist()
+
+# diagonal persistence diagram using the `flat = FALSE` parameter method
 ggplot(toy.data,
        aes(start = birth, end = death, colour = dim, shape = dim)) +
-  geom_persistence(stat = "flat")
-ggplot(toy.data,
-       aes(start = birth, end = death, colour = dim, shape = dim)) +
-  stat_flat(geom = "persistence")
-# flat persistence diagram using the `flat = TRUE` parameter method
-ggplot(toy.data,
-       aes(start = birth, end = death, colour = dim, shape = dim)) +
-  geom_persistence(flat = TRUE)
+  geom_persistence(flat = FALSE)
 
 # flat persistence diagram using the geom layer with minimalist theme
 ggplot(toy.data,
        aes(start = birth, end = death, colour = dim, shape = dim)) +
-  geom_persistence(stat = "flat") +
+  geom_persistence() +
   theme_tda()
 
 #####EXAMPLE 2#####
 # load library and dataset for comprehensive example
 library("TDAstats")
-data("annulus2d") # noisy unit circle (betti-1 number = 1)
+data("annulus2d")  # noisy unit circle (betti-1 number = 1)
 
 # calculate persistence homology and format
 annulus.phom <- as.data.frame(calculate_homology(annulus2d))
 annulus.phom$dimension <- as.factor(annulus.phom$dimension)
 
+# pretty flat persistence diagram
+ggplot(annulus.phom, aes(start = birth, end = death,
+                         shape = dimension, colour = dimension)) +
+  geom_persist() +
+  theme_persist()
+
 # pretty diagonal persistence diagram
 ggplot(annulus.phom, aes(start = birth, end = death,
                          shape = dimension, colour = dimension)) +
-  geom_persistence() +
-  theme_persist()
-
-# another way to get a pretty flat persistence diagram
-ggplot(annulus.phom, aes(start = birth, end = death,
-                         shape = dimension, colour = dimension)) +
-  geom_persistence(stat = "flat") +
-  theme_persist()
-ggplot(annulus.phom, aes(start = birth, end = death,
-                         shape = dimension, colour = dimension)) +
-  geom_persistence(flat = TRUE) +
+  geom_persistence(flat = FALSE) +
   theme_persist()
