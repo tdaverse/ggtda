f9df2cccaa416635517b69e71b1d388f2b8092ae
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-01-27 17:01:36 -0500

    Added coverage for internal function

--- tests/testthat/test_vietoris.R
+++ tests/testthat/test_vietoris.R
@@ -1,6 +1,24 @@
 context("Test visualization of Vietoris-Rips diagrams")
 library("ggtda")
 
+test_that("Vietoris-Rips distance calculations run fine", {
+  # set seed for reproducibility
+  set.seed(42)
+  
+  # generate pseudorandom dataset (points from noisy circle)
+  angles <- runif(10, 0, 2 * pi)
+  annulus <- cbind(x = cos(angles) + rnorm(10, 0, 0.1),
+                   y = sin(angles) + rnorm(10, 0, 0.1))
+  
+  # correct output and check for match (row permutation irrelevant so sort first)
+  expected <- matrix(c(1, 2, 5, 9, 9, 10), ncol = 2, byrow = TRUE)
+  colnames(expected) <- c("a", "b")
+  output <- proximate_pairs(annulus, diameter = 0.3)
+  output <- output[order(output[, 1]), ]
+  
+  expect_equal(expected, output)
+})
+
 test_that("Vietoris-Rips geom(s) and stat(s) work correctly", {
   # sample dataset - noisy circle
   angles <- runif(100, 0, 2 * pi)
