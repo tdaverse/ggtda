edd19f73f4e21291f056b1494cc74bc5cbeea0d3
Raoul Wadhwa <raoulwadhwa@gmail.com>
2019-12-23 21:07:03 -0500

    add param to theme_barcode

--- R/themes.R
+++ R/themes.R
@@ -37,16 +37,24 @@ theme_persist <- function() {
 #' the form of persistence barcodes (aka topological barcodes).
 #' 
 #' @import ggplot2
+#' @param vertical  defaults to `FALSE` (no vertical axis); if `TRUE`, allows
+#'   users to more easily count the number of features plotted in barcode
+#'   (if `vertical` equals `TRUE`, this function is equivalent to `theme_tda`)
 #' @family TDA themes
 #' @export
-theme_barcode <- function() {
-  # same as theme_tda() without the vertical axis
-  theme(axis.line.x = element_line(colour = "black"),
-        axis.line.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.text.x = element_text(colour = "black"),
-        axis.text.y = element_blank(),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank())
+theme_barcode <- function(vertical = FALSE) {
+  # allows users to count how many features are there in barcode
+  if (vertical) {
+    theme_tda()
+  # default: same as theme_tda() without the vertical axis
+  } else {
+    theme(axis.line.x = element_line(colour = "black"),
+          axis.line.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.x = element_text(colour = "black"),
+          axis.text.y = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank())
+  }
 }
