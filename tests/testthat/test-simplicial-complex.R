9cdd746076d5072d8aed46d2030065057ef8eb67
corybrunson <cornelioid@gmail.com>
2020-10-31 13:18:35 -0400

    test(simplicial-complex): reorganize tests, remove one calculation test, rename

--- tests/testthat/test-simplicial-complex.R
+++ tests/testthat/test-simplicial-complex.R
@@ -1,7 +1,6 @@
-context("Test generation and visualization of simplicial complexes")
-library("ggtda")
+context("Computation and visualization of simplicial complexes")
 
-test_that("Proximate functions work as expected on equilateral triangle", {
+test_that("proximate functions threshold correctly on equilateral triangle", {
   
   # equilateral triangle
   et <- data.frame(x = cos(2*pi*c(0,1/3,2/3)), y = sin(2*pi*c(0,1/3,2/3)))
@@ -17,49 +16,28 @@ test_that("Proximate functions work as expected on equilateral triangle", {
   expect_equal(nrow(proximate_triples(et, diameter = 2 + eps)), 1L)
 })
 
-test_that("Vietoris-Rips distance calculations run as expected", {
-  
-  # pseudorandom data (points from noisy circle) w/ seed for reproducibility
-  set.seed(42)
-  angles <- runif(10, 0, 2 * pi)
-  annulus <- cbind(x = cos(angles) + rnorm(10, 0, 0.1),
-                   y = sin(angles) + rnorm(10, 0, 0.1))
-  
-  # proximate pairs (row permutation irrelevant so sort first)
-  output <- proximate_pairs(annulus, diameter = 0.3)
-  output <- output[order(output[, 1]), ]
-  
-  # expected output
-  expected <- matrix(c(1, 2, 5, 9, 9, 10), ncol = 2, byrow = TRUE)
-  colnames(expected) <- c("a", "b")
-  
-  # ensure equal
-  expect_equal(expected, output)
-})
-
 # sample data set
 d <- as.data.frame(ggtda::annulus2d)
 
-test_that("Disk layer works as expected", {
+test_that("disk layer works as expected", {
   
   # disks of specified radius and resolution (segments)
   p <- ggplot(d, aes(x = x, y = y)) +
     geom_point() +
-    stat_disk(radius = 0.35, segments = 60, fill = "aquamarine3")
+    stat_disk(radius = 0.35, segments = 60L, fill = "aquamarine3")
   expect_is(p, "ggplot")
   expect_is(p$layer[[1]], "ggproto")
   expect_equal(c(p$labels$x, p$labels$y), c("x", "y"))
-  # throws error when tested by CI tools
-  #expect_equal(nrow(layer_data(p, 2)), nrow(d) * (60 + 1))
   
   # skip on continuous integration services
   skip_on_travis()
   skip_on_appveyor()
   
+  # throws error when tested by CI tools
+  expect_equal(nrow(layer_data(p, i = 2L)), nrow(d) * (60L + 1L))
+  
   # visual regression test
-  vdiffr::expect_doppelganger(
-    "Fixed-radius disks around annulus data set", p, "simplicial-complex"
-  )
+  vdiffr::expect_doppelganger("stat_disk, annulus", p)
 })
 
 test_that("Cech layers work as expected", {
@@ -93,15 +71,9 @@ test_that("Cech layers work as expected", {
   skip_on_appveyor()
   
   # visual regression tests
-  vdiffr::expect_doppelganger(
-    "Cech 0-skeleton on annulus data set", p0, "simplicial-complex"
-  )
-  vdiffr::expect_doppelganger(
-    "Cech 1-skeleton on annulus data set", p1, "simplicial-complex"
-  )
-  vdiffr::expect_doppelganger(
-    "Cech 2-skeleton on annulus data set", p2, "simplicial-complex"
-  )
+  vdiffr::expect_doppelganger("stat_cech0, annulus", p0)
+  vdiffr::expect_doppelganger("stat_cech1, annulus", p1)
+  vdiffr::expect_doppelganger("stat_cech2, annulus", p2)
 })
 
 test_that("Vietoris layers work as expected", {
@@ -135,13 +107,7 @@ test_that("Vietoris layers work as expected", {
   skip_on_appveyor()
   
   # visual regression tests
-  vdiffr::expect_doppelganger(
-    "Vietoris 0-skeleton on annulus data set", p0, "simplicial-complex"
-  )
-  vdiffr::expect_doppelganger(
-    "Vietoris 1-skeleton on annulus data set", p1, "simplicial-complex"
-  )
-  vdiffr::expect_doppelganger(
-    "Vietoris 2-skeleton on annulus data set", p2, "simplicial-complex"
-  )
+  vdiffr::expect_doppelganger("stat_vietoris0, annulus", p0)
+  vdiffr::expect_doppelganger("stat_vietoris1, annulus", p1)
+  vdiffr::expect_doppelganger("stat_vietoris2, annulus", p2)
 })
--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -6,12 +6,15 @@ test_that("provided themes explicitly define only (void) theme elements", {
   t0 <- theme_void()
   
   t <- theme_tda()
+  expect_is(t, "theme")
   expect_true(all(names(t) %in% names(t0)))
   
   t <- theme_persist()
+  expect_is(t, "theme")
   expect_true(all(names(t) %in% names(t0)))
   
   t <- theme_barcode()
+  expect_is(t, "theme")
   expect_true(all(names(t) %in% names(t0)))
 })
 
@@ -27,16 +30,16 @@ test_that("themes don't change without acknowledgement", {
     facet_wrap(~ cloud)
   # comparisons
   skip_on_cran()
-  vdiffr::expect_doppelganger("theme_tda_persist", p + theme_tda())
-  vdiffr::expect_doppelganger("theme_persist_persist", p + theme_persist())
+  vdiffr::expect_doppelganger("theme_tda, persistence", p + theme_tda())
+  vdiffr::expect_doppelganger("theme_persist, persistence", p + theme_persist())
   # element-rich barcode plot
   p <- ggplot(df, aes(start = birth, end = death, color = dimension)) +
     geom_barcode() +
     facet_wrap(~ cloud)
   # comparisons
   skip_on_cran()
-  vdiffr::expect_doppelganger("theme_tda_barcode", p + theme_tda())
-  vdiffr::expect_doppelganger("theme_barcode_barcode", p + theme_barcode())
-  vdiffr::expect_doppelganger("theme_barcode_barcode_vertical",
+  vdiffr::expect_doppelganger("theme_tda, barcode", p + theme_tda())
+  vdiffr::expect_doppelganger("theme_barcode, barcode", p + theme_barcode())
+  vdiffr::expect_doppelganger("theme_barcode, barcode, vertical",
                               p + theme_barcode(vertical = TRUE))
 })
