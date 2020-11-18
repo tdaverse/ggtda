d5412fbb972106fa25eee8d777b9c5c6553987c9
Raoul W <raoulwadhwa@gmail.com>
2020-11-18 09:26:54 -0500

    dropped theme_tda

--- R/themes.R
+++ R/themes.R
@@ -1,23 +1,3 @@
-#' Theme for Persistent Homology Visualizations
-#' 
-#' A simple theme to clearly display persistent homology.
-#' This minimalistic theme omits plot components that are
-#' unnecessary in the context of persistent homology
-#' (e.g. gridlines, background color).
-#' The `theme_tda` function should get you close to a publication-quality
-#' plot
-#' 
-#' @import ggplot2
-#' @family TDA themes
-#' @export
-theme_tda <- function() {
-  theme(axis.line = element_line(colour = "black"),
-        axis.text = element_text(colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank())
-}
-
 #' Theme for Persistence Diagrams
 #' 
 #' A simple theme to clearly display persistent homology in
@@ -27,8 +7,11 @@ theme_tda <- function() {
 #' @family TDA themes
 #' @export
 theme_persist <- function() {
-  # no real differences (vertical axis is needed)
-  theme_tda()
+  theme(axis.line = element_line(colour = "black"),
+        axis.text = element_text(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank())
 }
 
 #' Theme for Persistence Barcodes
