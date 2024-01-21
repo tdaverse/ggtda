e159498775aff9b93bb35f544a0104a40395291a
Jason Cory Brunson <cornelioid@gmail.com>
2024-01-21 17:22:28 -0500

    debug dataset example

--- inst/examples/ex-persistence-dataset.R
+++ inst/examples/ex-persistence-dataset.R
@@ -1,11 +1,12 @@
 
+if ("ripserr" %in% rownames(utils::installed.packages())) {
 # this is silly but a good test
 raw_data <- data.frame(obj = I(list(eurodist, 10*swiss, Nile)))
 raw_data$class <- vapply(raw_data$obj, class, "")
 # exclude time series data if {ripserr} v0.1.1 is installed
 if ("ripserr" %in% rownames(utils::installed.packages()) &&
     utils::packageVersion("ripserr") == "0.1.1")
-  raw_data[c(1L, 2L), ]
+  raw_data <- raw_data[c(1L, 2L), ]
 # persistence diagram
 ggplot(raw_data, aes(dataset = obj)) +
   stat_persistence(aes(color = factor(after_stat(dimension)), shape = class))
@@ -14,3 +15,4 @@ ggplot(raw_data, aes(dataset = obj)) +
   facet_wrap(facets = vars(class), scales = "free") +
   stat_landscape(aes(color = factor(after_stat(dimension)))) +
   theme(legend.position = "bottom")
+}
