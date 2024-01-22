4989fa0982bb3e271b71d88e76571245515fe2eb
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2024-01-22 11:48:08 -0500

    explicitly import rlang functions to preempt warning on check

--- R/ggtda.R
+++ R/ggtda.R
@@ -13,20 +13,21 @@ NULL
 #'
 #' @import ggplot2
 #' @name ggtda
+#' @importFrom rlang on_load run_on_load
 #' @docType package
 NULL
 
 # installed versions of {simplextree} and {ripserr}, or `NULL` if not installed
 .onLoad <- function(...) {
-  rlang::run_on_load()
+  run_on_load()
 }
-rlang::on_load(
+on_load(
   .simplextree_version <-
     if ("simplextree" %in% rownames(utils::installed.packages())) {
       utils::packageVersion("simplextree")
     } else NA_character_
 )
-rlang::on_load(
+on_load(
   .ripserr_version <-
     if ("ripserr" %in% rownames(utils::installed.packages())) {
       utils::packageVersion("ripserr")
